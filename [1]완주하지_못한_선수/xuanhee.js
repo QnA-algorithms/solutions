c8aa466af0129f8b89c6ad2eaf8fafa7f160eb31
seonhwa oh <76085705+xuanhee16@users.noreply.github.com>
2021-12-12 12:36:36 +0900

    [1]완주하지_못한_선수_xuanhee.js

---
+++ [1]완주하지_못한_선수/xuanhee.js
@@ -0,0 +1,9 @@
+function solution(participant, completion) { 
+    let par = participant.sort();
+    let com = completion.sort();
+    for(let i = 0; i < par.length; i++){
+      if(par[i] !== com[i]){
+      return par[i];   
+      }        
+    } 
+}
